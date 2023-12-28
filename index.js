a84b6f88bccb6361da0204522ddb0a5b0d9d35c4
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2023-12-29 00:57:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -111,9 +111,9 @@ try{
 			await delay(1999)
             console.log(chalk.yellow(`\n\n               ${chalk.bold.blue(`[ ${botname} ]`)}\n\n`))
             console.log(color(`< ================================================== >`, 'cyan'))
-	        console.log(color(`\n${themeemoji} YT CHANNEL: Xeon`,'magenta'))
-            console.log(color(`${themeemoji} GITHUB: DGXeon `,'magenta'))
-            console.log(color(`${themeemoji} INSTAGRAM: @unicorn_xeon `,'magenta'))
+	        console.log(color(`\n${themeemoji} YT CHANNEL: Yuji`,'magenta'))
+            console.log(color(`${themeemoji} GITHUB: Yuujikun `,'magenta'))
+            console.log(color(`${themeemoji} INSTAGRAM: @black_yuuji `,'magenta'))
             console.log(color(`${themeemoji} WA NUMBER: ${owner}`,'magenta'))
             console.log(color(`${themeemoji} CREDIT: ${wm}\n`,'magenta'))
 		}
