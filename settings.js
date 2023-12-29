54b6c845dacd3be4261a2ad3aa8810ba7e0e2dc1
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2023-12-29 07:55:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['2348153995521'] //ur owner number
+global.ownernomer = "2348153995521" //ur owner number2
+global.ownername = "🦄Blac Yuji" //ur owner name
+global.ytname = "YT: Blacyuuji" //ur yt chanel name
+global.socialm = "GitHub: Yuujikun" //ur github or insta name
+global.location = Nigeria, Kola, AIT Rd" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botname = "Mechamaru"
+global.ownernumber = '2348153995521'
+global.ownername = '🦄드림 가이 Yuji'
+global.ownerNumber = ["2348153995521@s.whatsapp.net"]
+global.ownerweb = "https://whatsapp.com/channel/0029Va8VuWo4o7qRFa9pEF47"
+global.websitex = "https://whatsapp.com/channel/0029Va8VuWo4o7qRFa9pEF47"
+global.wagc = "https://chat.whatsapp.com/HMufD1Buoqo6gOOMA1KHEp"
+global.themeemoji = '🧸'
+global.wm = "Jujutsu Bot Inc."
+global.botscript = 'https://github.com/Yuujikun/Jujutsu-Bot/' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 Yuji\n\n"
+global.creator = "2348153995521@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
